eafbe8f3d2cdddc8d16d475efa08ea0abdcc3a5b
olgazh565 <olgazh565@gmail.com>
2023-09-14 17:12:10 +0300

    sass import fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
-import sassPkg from 'sass';
+import * as sassPkg from 'sass';
 import gulpSass from 'gulp-sass';
 import {deleteSync} from 'del';
 
